7be2894cf086058da569a67b0fc2f66dcb2c8030
0tae <choiyt3465@naver.com>
2025-03-18 21:55:48 +0900

    feat : 예약 목록 불러오기 서비스 로직 구현

--- src/main/java/com/example/hospitalreservation/service/ReservationService.java
+++ src/main/java/com/example/hospitalreservation/service/ReservationService.java
@@ -1,20 +1,23 @@
 package com.example.hospitalreservation.service;
 
 import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-// TODO : 서비스 레이어에서 필요한 어노테이션을 작성해주세요.
 @Service
 public class ReservationService {
 
-    // TODO : 주입 받아야 객체를 작성해주세요.
+    private final ReservationRepository reservationRepository;
 
-    // TODO : 모든 예약 리스트를 조회하는 코드를 작성해주세요.
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
     public List<Reservation> getAllReservations() {
-        return null;
+        return reservationRepository.findAll();
     }
 
     // TODO : 새로운 예약을 생성하는 코드를 작성해주세요.
