c9ee4cfb14b4b3c43aec5119dd4fd06ccc0d33c1
0tae <choiyt3465@naver.com>
2025-03-18 21:41:04 +0900

    feat : View 반환 경로 설정

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -1,35 +1,36 @@
 package com.example.hospitalreservation.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-// TODO : 컨트롤러에 필요한 어노테이션을 작성해주세요.
-// TODO : 요청 경로는 templates를 참고하여 작성해주세요.
+@Controller
 public class ReservationController {
 
     // TODO : 주입 받아야 할 객체를 설정해주세요.
 
-    // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping("/reservations")
     public String getReservations(Model model) {
         // TODO : 예약 메인 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        // TODO : 예약 목록을 가져오는 코드 작성
+        return "index.html";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping("/reservation_form")
     public String showReservationForm() {
-        // TODO : 예약하기 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        return "reservation_form.html";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
     public String createReservation(@RequestParam Long doctorId, @RequestParam Long patientId) {
         // TODO : 예약을 진행하는 코드를 작성해주세요.
-        return null;
+        return "index.html";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
     public String cancelReservation(@PathVariable Long id) {
         // TODO : 예약을 취소하는 코드를 작성해주세요.
-        return null;
+        return "index.html";
     }
 }
