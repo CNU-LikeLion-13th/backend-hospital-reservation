4972021fa5aaf89c6aa9289897bb4c38ffdb12d4
0tae <choiyt3465@naver.com>
2025-03-19 11:47:30 +0900

    feat : Reservation model 정의

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -3,10 +3,36 @@ package com.example.hospitalreservation.model;
 import java.time.LocalDateTime;
 
 public class Reservation {
+    private static long ID_SEQ = 1;
     private Long id;
     private Long doctorId;
     private Long patientId;
     private LocalDateTime reservationTime;
 
-    // TODO : 필요한 메서드가 있다면 작성해주세요.
+    private Reservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
+        this.id = ID_SEQ++;
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.reservationTime = reservationTime;
+    }
+
+    public static Reservation of(Long doctorId, Long patientId, LocalDateTime reservationTime){
+        return new Reservation(doctorId,patientId,reservationTime);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public LocalDateTime getReservationTime() {
+        return reservationTime;
+    }
 }
