fba55a6679d19665589ebe50ae3e2a91c852d202
lye5615 <lye5615@naver.com>
2025-03-24 01:03:28 +0900

    feat: 모든 예약 엔티티 조회 기능 추가

--- src/main/java/com/example/hospitalreservation/repository/ReservationRepository.java
+++ src/main/java/com/example/hospitalreservation/repository/ReservationRepository.java
@@ -12,17 +12,17 @@ public class ReservationRepository {
     private final List<Reservation> reservations = new ArrayList<>();
     private Long nextId = 1L;
 
-    // TODO : 모든 예약 엔티티를 조회하는 코드를 작성해주세요.
+    // TODO_w2 : 모든 예약 엔티티를 조회하는 코드를 작성해주세요.
     public List<Reservation> findAll() {
-        return null;
+        return new ArrayList<>(reservations);
     }
 
-    // TODO : 예약 엔티티를 저장하는 코드를 작성해주세요.
+    // TODO_w2 : 예약 엔티티를 저장하는 코드를 작성해주세요.
     public Reservation save(Reservation reservation) {
         return null;
     }
 
-    // TODO : 예약 엔티티를 삭제하는 코드를 작성해주세요.
+    // TODO_w2 : 예약 엔티티를 삭제하는 코드를 작성해주세요.
     public void deleteById(Long id) {
         return;
     }
