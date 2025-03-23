76d14a3f98f2e8a472f60f6e48d65a0723b6ba80
lye5615 <lye5615@naver.com>
2025-03-24 01:52:59 +0900

    feat: 모든 예약 리스트 조회 기능 추가

--- src/main/java/com/example/hospitalreservation/service/ReservationService.java
+++ src/main/java/com/example/hospitalreservation/service/ReservationService.java
@@ -1,25 +1,34 @@
 package com.example.hospitalreservation.service;
 
 import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-// TODO : 서비스 레이어에서 필요한 어노테이션을 작성해주세요.
+// TODO_w2 : 서비스 레이어에서 필요한 어노테이션을 작성해주세요.
 @Service
 public class ReservationService {
 
-    // TODO : 주입 받아야 객체를 작성해주세요.
-
+    // TODO_w2 : 주입 받아야 객체를 작성해주세요.
+    private final ReservationRepository reservationRepository;
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
     // TODO : 모든 예약 리스트를 조회하는 코드를 작성해주세요.
     public List<Reservation> getAllReservations() {
-        return null;
+        return reservationRepository.findAll();
     }
 
     // TODO : 새로운 예약을 생성하는 코드를 작성해주세요.
     public Reservation createReservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
-        return null;
+        Reservation reservation = new Reservation();
+        reservation.setDoctorId(doctorId);
+        reservation.setPatientId(patientId);
+        reservation.setReservationTime(reservationTime);
+
+        return reservationRepository.save(reservation);
     }
 
     // TODO : 예약을 취소하는 코드를 작성해주세요.
