04e6bb7f2a33bde04993b5ee6a8841a71cf8354a
0-tae <choiyt3465@naver.com>
2025-03-30 04:21:16 +0900

    refactor : API 응답 반환타입 수정 및 에러 핸들링 구현 - 각 API의 dto를 반환하도록 수정 - @ExceptionHandler 사용하여 에러 메시지를 반환하도록 수정

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -1,14 +1,12 @@
 package com.example.hospitalreservation.controller;
 
-import com.example.hospitalreservation.model.CreateReservationRequest;
-import com.example.hospitalreservation.model.DeleteReservationRequest;
-import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.model.*;
 import com.example.hospitalreservation.service.ReservationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -35,15 +33,22 @@ public class ReservationController {
     }
 
     @PostMapping
-    public String createReservation(@ModelAttribute CreateReservationRequest dto) {
-        Reservation reservation = reservationService.createReservation(dto);
-        return "redirect:/reservations?reservationId="+reservation.getId();
+    @ResponseBody
+    public ResponseEntity<CreateReservationResponse> createReservation(@ModelAttribute CreateReservationRequest dto) {
+        CreateReservationResponse response = reservationService.createReservation(dto);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/delete/{id}")
-    public String cancelReservation(@PathVariable Long id,
-                                    @ModelAttribute DeleteReservationRequest request) {
-        reservationService.cancelReservation(id, request);
-        return "redirect:/reservations";
+    @ResponseBody
+    public ResponseEntity<DeleteReservationResponse> cancelReservation
+            (@PathVariable Long id, @ModelAttribute DeleteReservationRequest request) {
+        DeleteReservationResponse response = reservationService.cancelReservation(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public  ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e.getMessage()));
     }
 }
