9b191f78941e5fc953d17409241b95166c2560e2
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-03-30 16:51:36 +0900

    update: 불필요한 생성자 제거

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -9,14 +9,9 @@ public class Reservation {
     private final LocalDateTime reservationTime;
 
     // TODO : 필요한 메서드가 있다면 작성해주세요.
-    public Reservation(Long id, Long doctorId, Long patientId, LocalDateTime reservationTime) {
-        this.id = id;
-        this.doctorId = doctorId;
-        this.patientId = patientId;
-        this.reservationTime = reservationTime;
-    }
 
     public Reservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
+
         this.doctorId = doctorId;
         this.patientId = patientId;
         this.reservationTime = reservationTime;
