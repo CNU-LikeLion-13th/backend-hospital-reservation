ab7f090c5ed07af2eae80af47bd95a17411f9637
0tae <choiyt3465@naver.com>
2025-04-05 22:49:49 +0900

    feat : 진료비용 및 비용 인터페이스 정의

---
+++ src/main/java/com/example/hospitalreservation/service/treatmentfees/TreatmentsFee.java
@@ -0,0 +1,8 @@
+package com.example.hospitalreservation.service.treatmentfees;
+
+import com.example.hospitalreservation.enums.Treatments;
+
+public interface TreatmentsFee extends Fee {
+
+    Treatments getTreatment();
+}
