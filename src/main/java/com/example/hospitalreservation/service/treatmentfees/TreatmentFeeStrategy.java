103d3f39cc49858e6a7b462e3419d9348d2c849c
0tae <choiyt3465@naver.com>
2025-04-05 22:52:27 +0900

    feat : 진료비 계산 전략을 처리하는 TreatmentFeeStrategy 구현 - TreatmentsFee 구현체를 Collection Injection으로 주입

---
+++ src/main/java/com/example/hospitalreservation/service/treatmentfees/TreatmentFeeStrategy.java
@@ -0,0 +1,29 @@
+package com.example.hospitalreservation.service.treatmentfees;
+
+import com.example.hospitalreservation.enums.Treatments;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+public class TreatmentFeeStrategy {
+
+    private final ConcurrentMap<Treatments, TreatmentsFee> calculateStrategyMap;
+
+
+    // Collection Injection 사용
+    // TreatmentsFeeCalculateStrategy를 구현한 모든 컴포넌트를 List Collection안에 넣는 DI 기법이라고 합니다.
+    // 처음 써보게 되어 기록합니다.
+    public TreatmentFeeStrategy(List<TreatmentsFee> treatmentsFees) {
+        this.calculateStrategyMap = new ConcurrentHashMap<>();
+
+        treatmentsFees.forEach(treatmentFee ->
+                calculateStrategyMap.put(treatmentFee.getTreatment(),treatmentFee));
+    }
+
+    public TreatmentsFee getTreatmentFee(String reason){
+        return calculateStrategyMap.get(Treatments.from(reason));
+    }
+}
